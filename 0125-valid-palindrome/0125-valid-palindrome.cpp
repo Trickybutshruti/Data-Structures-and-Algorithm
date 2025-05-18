52a498bc1a8703ade5f270f83769226c82360564
Shruti Sinha <135057264+Trickybutshruti@users.noreply.github.com>
2025-05-18 12:39:25 +0530

    Time: 0 ms (100%), Space: 10.2 MB (28.08%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned;
+        for (char c : s) {
+            if (isalnum(c)) {
+                cleaned += tolower(c);
+            }
+        }
+        int start=0;
+        int end=cleaned.size()-1;
+        while(start<end){
+            if(cleaned[start]==cleaned[end]){
+                start++;
+                end--;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
